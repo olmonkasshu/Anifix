5bde40f89f643613471c6f2495e0f6ba8722246f
olmonkasshu <olmontechdesign@gmail.com>
2024-02-12 15:58:46 +0100

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import HeroPage from "./Components/Hero/HeroPage"
 import Footer from "./Components/Footer/Footer"
 
 import MyCard from "./Components/Card/MyCard"
+import UpComing from "./Components/Card/UpComing"
 
 function App() {
   return (
    <div>
       <HeroPage/>
       <MyCard />
+      <UpComing />
       <Footer />
    </div>
   )
---
+++ src/Components/Card/UpComing.jsx
@@ -0,0 +1,58 @@
+export default function UpComing() {
+    return (
+      <div>
+        <h3 className="ml-5 text-xl font-extrabold  md:ml-4 relative text-md w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-red-700 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">Up Coming</h3>
+        <div className="flex overflow-x-auto">
+          <div className="relative w-52 h-auto m-5 hover:opacity-100">
+            <img src="src/assets/Poster/90767df5dd68374dbddfa847325c7ba2_092d1b87dff0e8d5f4e1b7f54906d306.jpg" alt="" />
+            <div className="absolute bottom-0 left-0 right-0 bg-red-700 opacity-50 p-2 hover:opacity-100">
+              <h1 className="text-white font-extrabold z-10 text-xs">VinLand Sega</h1>
+            </div>
+          </div>
+  
+          <div className="relative w-52 h-auto m-5 hover:opacity-100">
+            <img src="src/assets/Poster/a9bb70e5241fd0c62bedc4fd068477f4_de227d92b2501750fec67f03c7b45ec3.jpg" alt="" />
+            <div className="absolute bottom-0 left-0 right-0 bg-red-700 opacity-50 p-2 hover:opacity-100">
+              <h1 className="text-white font-extrabold z-10 text-xs">Berserk</h1>
+            </div>
+          </div>
+  
+          <div className="relative w-52 h-auto m-5 hover:opacity-100">
+            <img src="src/assets/Poster/b2fb4225c445027f050094ab3c21e2e9_f821b0e67774883cd4960df0abb80fb5.webp" alt="" />
+            <div className="absolute bottom-0 left-0 right-0 bg-red-700 opacity-50 p-2 hover:opacity-100">
+              <h1 className="text-white font-extrabold z-10 text-xs">Demon Slayer</h1>
+            </div>
+          </div>
+  
+          <div className="relative w-52 h-auto m-5 hover:opacity-100">
+            <img src="src/assets/Poster/e97831f401c1bb33c74f707c8318c139_f194e34be40330216dbceebdd91a7e40.webp" alt="" />
+            <div className="absolute bottom-0 left-0 right-0 bg-red-700 opacity-50 p-2 hover:opacity-100">
+              <h1 className="text-white font-extrabold z-10 text-xs">Neon Genesis Evangelion</h1>
+            </div>
+          </div>
+  
+          <div className="relative w-52 h-auto m-5 hover:opacity-100">
+            <img src="src/assets/Poster/d5a25bc779f26c4de57246331798de81_6e4dd8f8cdc97fc7f77924a95f513503.webp" alt="" />
+            <div className="absolute bottom-0 left-0 right-0 bg-red-700 opacity-50 p-2 hover:opacity-100">
+              <h1 className="text-white font-extrabold z-10 text-xs">Black Clover</h1>
+            </div>
+          </div>
+  
+          <div className="relative w-52 h-auto m-5 hover:opacity-100">
+            <img src="src/assets/Poster/d98d5069827ecfe1fdd9f64d44e19f3b_0fec25f4bc673e87ed11c3ad49b84cee.jpg" alt="" />
+            <div className="absolute bottom-0 left-0 right-0 bg-red-700 opacity-50 p-2 hover:opacity-100">
+              <h1 className="text-white font-extrabold z-10 text-xs">OverLord</h1>
+            </div>
+          </div>
+  
+          <div className="relative w-52 h-auto m-5 hover:opacity-100">
+            <img src="src/assets/Poster/dc3a148b09642cc32287f0e49d66c377_3c6a322a6f3acd5815dde3699a012fb1.webp" alt="" />
+            <div className="absolute bottom-0 left-0 right-0 bg-red-700 opacity-50 p-2 hover:opacity-100">
+              <h1 className="text-white font-extrabold z-10 text-xs">My Hero Academia</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
